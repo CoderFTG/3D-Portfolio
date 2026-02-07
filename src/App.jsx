37b171ba9bf050bd7f1048924263c90f658fac77
Tanishq Gaur <tanishq.gaur07@gmail.com>
2026-02-07 23:38:23 +0530

    implement feature cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Hero } from './sections/Hero'
 import { ShowcaseSection } from './sections/ShowcaseSection'
+import FeatureCards from './sections/FeatureCards'
 
 const App = () => {
     return (
         <>
             <Hero />
             <ShowcaseSection />
+            <FeatureCards/>
         </>
     )
 }
